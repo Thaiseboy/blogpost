d371da5db01e03362159ee7b982911ca84ae6ba6
Thaiseboy <get_sarun@hotmail.com>
2025-02-21 01:21:16 +0100

    First page for testing

--- index.php
+++ index.php
@@ -15,27 +15,21 @@ $posts = $stmt->fetchAll();
 </head>
 <body>
     <nav class="navbar navbar-expand-lg navbar-dark bg-dark">
-        <a href="#" class="text-center">Blogpost</a>
-
-<?php
-require_once "config.php";
-
-$stmt = $pdo->query("SELECT * FROM blogpost");
-$stmt->fetchAll();
-?>
-        <div class="container mt-4">
-            <h1 class="text-center">Welkom op mijn Blog!</h1>
-<?php foreach($posts as $post): ?>
-<div class="card mb-3">
-    <dic class="card-body">
-        <h5 class="card-title"> <?= htmlspecialchars($post['title']) ?></h5>
-        <p class="card-text"><?= nl2br(htmlspecialchars($post["content"])) ?></p>
-        <p class="text-muted">Geplaatst op: <?= $post['create_at'] ?></p>
-    </dic>
-</div>
-<?php endforeach ?>
-        </div>
+        <a class="navbar-brand" href="#">Mijn Blog</a>
     </nav>
+    <div class="container mt-4">
+        <h1 class="text-center">Welkom op mijn Blog!</h1>
+        
+        <?php foreach ($posts as $post): ?>
+            <div class="card mb-3">
+                <div class="card-body">
+                    <h5 class="card-title"><?= htmlspecialchars($post['title']) ?></h5>
+                    <p class="card-text"><?= nl2br(htmlspecialchars($post['content'])) ?></p>
+                    <p class="text-muted">Geplaatst op: <?= $post['created_at'] ?></p>
+                </div>
+            </div>
+        <?php endforeach; ?>
+    </div>
     <script src="https://code.jquery.com/jquery-3.5.1.slim.min.js"></script>
     <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/js/bootstrap.bundle.min.js"></script>
 </body>
