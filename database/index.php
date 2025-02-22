9290ebce70661528737744440ee78adafc0d3074
Thaiseboy <get_sarun@hotmail.com>
2025-02-22 01:09:48 +0100

    Fix index

--- database/index.php
+++ database/index.php
@@ -1,10 +1,3 @@
-<?php
-require_once "config.php";
-
-$stmt = $pdo->query("SELECT * FROM posts ORDER BY created_at DESC");
-$posts = $stmt->fetchAll();
-?>
-
 <!DOCTYPE html>
 <html lang="nl">
 <head>
@@ -19,7 +12,7 @@ $posts = $stmt->fetchAll();
     </nav>
     <div class="container mt-4">
         <h1 class="text-center">Welkom op mijn Blog!</h1>
-        
+
         <?php foreach ($posts as $post): ?>
             <div class="card mb-3">
                 <div class="card-body">
@@ -29,8 +22,7 @@ $posts = $stmt->fetchAll();
                 </div>
             </div>
         <?php endforeach; ?>
+
     </div>
-    <script src="https://code.jquery.com/jquery-3.5.1.slim.min.js"></script>
-    <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/js/bootstrap.bundle.min.js"></script>
 </body>
 </html>
